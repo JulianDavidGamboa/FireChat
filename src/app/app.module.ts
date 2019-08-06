96a41860ac6fa48a1579d60fe2fef4632dc9283a
Julian Gamboa <juliandg34@gmail.com>
2019-08-06 14:05:17 -0500

    Login with Google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,20 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-
 import { environment } from '../environments/environment';
-import { ChatComponent } from './components/chat/chat.component';
 
+// Componentes
+import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component';
+
+// Servicio
 import { ChatService } from './providers/chat.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,9 +1,79 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
+import { Mensaje } from '../interface/mensaje.interface';
+
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
+
+  public chats: Mensaje[] = [];
+  public usuario: any = {};
+
+  constructor( private afs: AngularFirestore, public afAuth: AngularFireAuth ) { 
+
+    this.afAuth.authState.subscribe(user => {
+
+      console.log( 'Estado del usuario: ', user );
+
+      if ( !user ) {
+        return;
+      }
+
+      this.usuario.nombre = user.displayName;
+      this.usuario.uid = user.uid;
+
+    });
+
+  }
+  
+  login( proveedor: string ) {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.usuario = {};
+    this.afAuth.auth.signOut();
+  }
+
+  cargarMensajes() {
+    
+    this.itemsCollection = this.afs.collection<Mensaje>('chats', ref => ref.orderBy('fecha','desc').limit(5));
+    return this.itemsCollection.valueChanges().pipe(
+      map((mensajes: Mensaje[]) => {
+        console.log(mensajes);
+        this.chats = [];
+
+        for ( let mensaje of mensajes ) {
+          this.chats.unshift( mensaje );
+        }
+        
+        return this.chats;
+      })
+    );
+
+  }
+
+  agregarMensaje( texto:string ) {
+
+    // TODO: Falta el uid del usuario
+    let mensaje: Mensaje = {
+
+      nombre: 'Demo',
+      mensaje: texto,
+      fecha: new Date().getTime()
+
+    }
+
+    return this.itemsCollection.add( mensaje );
+
+  }
 }
